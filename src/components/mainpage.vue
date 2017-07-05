33946e8906ca31b62838791e35baf94f5fd5db48
alansif <alansif@sina.com>
2017-07-05 22:41:15 +0800

    aaa

--- src/components/mainpage.vue
+++ src/components/mainpage.vue
@@ -1,23 +1,37 @@
 <template>
-    <div class="card">
-        <el-button type="primary" @click="go()">click</el-button>
-        <el-button type="primary" @click="getpre()">GetPreviousAnswer</el-button>
-        <template v-for="(question,idx) in questions">
-            <p class="ques" :class="hint && noe(ans[question.QID]) ? 'errcls': 'okcls'" :id="question.QID">{{question.QID}}.{{question.Question}}</p>
-            <checkboxs v-if="question.Multiple" :items="question.Alts" :qid="question.QID" :curselext="ans[question.QID]" @change="changed"></checkboxs>
-            <radios v-else :items="question.Alts" :qid="question.QID" :curselext="ans[question.QID]" @change="changed"></radios>
-        </template>
-      <hr style="margin-top:32px;height:1px;border:none;border-top:1px solid #ccc;" />
-      <div style="width:100%;margin-top:24px;text-align:center;">
-        <el-button type="primary" @click="nextstep">下一步</el-button>
-      </div>
+    <div>
+        <div style="width:200px;margin: 20px;">
+            <el-input v-model="idnumber"></el-input>
+            <el-input v-model="phone"></el-input>
+            <el-input v-model="vcode"></el-input>
+            <el-button @click="getverify">GetVerify</el-button>
+            <el-button @click="getlogin">Login</el-button>
+            <el-button @click="go">GetQuestion</el-button>
+            <el-button @click="getpre">GetPreviousAnswer</el-button>
+        </div>
+        <div class="card">
+            <div class="cardhead">
+                <div style="margin-left:32px;font-size:20px;">调查问卷</div>
+            </div>
+            <div class="cardbody">
+                <template v-for="(question,idx) in questions">
+                    <p class="ques" :class="hint && noe(ans[question.QID]) ? 'errcls': 'okcls'" :id="question.QID">{{question.QID}}.{{question.Question}}</p>
+                    <checkboxs v-if="question.Multiple" :items="question.Alts" :qid="question.QID" :curselext="ans[question.QID]" @change="changed"></checkboxs>
+                    <radios v-else :items="question.Alts" :qid="question.QID" :curselext="ans[question.QID]" @change="changed"></radios>
+                </template>
+                <hr style="margin-top:32px;height:1px;border:none;border-top:1px solid #ccc;" />
+                <div style="width:100%;margin-top:24px;text-align:center;">
+                    <el-button type="primary" @click="nextstep">下一步</el-button>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
     import radios from './radios.vue'
     import checkboxs from './checkboxs.vue'
-    
+
     function scrollElement(elmnt, duration) {
         var e = document.documentElement;
         if(e.scrollTop===0){
@@ -42,6 +56,9 @@
     export default {
         data () {
             return {
+                idnumber:'',
+                phone:'',
+                vcode:'',
                 questions: [],
                 ans:{},
                 hint:false
@@ -51,8 +68,32 @@
             noe(a) {
                 return !a || (a.length === 0);
             },
+            getverify() {
+                this.$http.post("http://111.198.146.40:3939/customize/MyService.asmx/GetVerifyInfo",{SFZH:this.idnumber,Phone:this.phone})
+                    .then((response)=>{
+                        let d = response.body.d;
+                        console.log(d);
+                    }, (response)=>{
+                        console.log(response);
+                    })
+                    .catch((response)=>{
+                        console.log(response);
+                    });
+            },
+            getlogin() {
+                this.$http.post("http://111.198.146.40:3939/customize/MyService.asmx/GetCustomerLoginInfo",{PaperValue:this.idnumber,Mobile:this.phone,PWD:this.vcode})
+                    .then((response)=>{
+                        let d = response.body.d;
+                        console.log(d);
+                    }, (response)=>{
+                        console.log(response);
+                    })
+                    .catch((response)=>{
+                        console.log(response);
+                    });
+            },
             go() {
-                this.$http.post("http://111.198.146.40:8082/customize/Survey.asmx/GetAllQuestion", {}).then((response) => {
+                this.$http.post("http://111.198.146.40:3939/customize/Survey.asmx/GetAllQuestion", {}).then((response) => {
                     var d = JSON.parse(response.body.d);
                     this.questions = d.data;
                     this.questions.forEach(q=>{this.ans[q.QID]=[]});
@@ -63,7 +104,7 @@
                 });
             },
             getpre() {
-                this.$http.post("http://111.198.146.40:8082/customize/Survey.asmx/GetPreviousAnswer", {id:'12345619880808HZ10'}).then((response) => {
+                this.$http.post("http://111.198.146.40:3939/customize/Survey.asmx/GetPreviousAnswer", {id:'12345619880808HZ10'}).then((response) => {
                     var d = JSON.parse(response.body.d);
                     if (d.status.code == 0) {
                         this.ans = d.data;
@@ -93,8 +134,6 @@
                 if (qid === null) return;
                 var elmnt = document.getElementById(qid);
                 scrollElement(elmnt, 300);
-//                console.log(document.documentElement.scrollTop);
-//                console.log(document.body.scrollTop);
                 this.hint = true;
                 this.$notify({
                   title: '提示',
@@ -126,13 +165,24 @@
     .card {
         margin:0 auto;
         width:900px;
-        padding: 32px 40px;
-        margin-bottom: 16px;
-        border: 1px solid #ddd;
+        padding-bottom:4px;
         border-radius: 10px;
+        box-shadow: 2px 2px 2px #888;
+        background-color: white;
+    }
+    .cardhead {
+        width:100%;
+        height:48px;
+        line-height:48px;
+        padding:0;
+        color:white;
+        background-color:rgb(100, 149, 237);
+        border-radius:10px 10px 0 0;
+    }
+    .cardbody {
+        padding: 16px 40px;
+        margin-bottom: 16px;
         color: #555;
-        background-color: #f7f7f7;
-        box-shadow: 1px 1px 1px #888;
     }
     .ques {
         font-size: 1.2em;
@@ -144,5 +194,8 @@
     }
     .errcls {
         color:red;
+    }
+    body {
+        background-color: #f3f3f3;
     }
 </style>
